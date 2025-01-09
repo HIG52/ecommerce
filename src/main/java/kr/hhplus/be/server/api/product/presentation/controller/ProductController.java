3fd278b86c3f71c7def9bcd0315bec7102bbea37
홍준영 <hongjy1997@naver.com>
2025-01-09 17:18:33 +0900

    :sparkles: feat : 스웨거 추가

--- src/main/java/kr/hhplus/be/server/api/product/presentation/controller/ProductController.java
+++ src/main/java/kr/hhplus/be/server/api/product/presentation/controller/ProductController.java
@@ -1,6 +1,12 @@
 package kr.hhplus.be.server.api.product.presentation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import kr.hhplus.be.server.api.balance.presentation.dto.BalanceResponseDTO;
 import kr.hhplus.be.server.api.coupon.domain.service.response.CouponsResponse;
 import kr.hhplus.be.server.api.coupon.presentation.dto.CouponsResponseDTO;
 import kr.hhplus.be.server.api.product.domain.service.ProductService;
@@ -22,11 +28,20 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Product API", description = "상품 관리 API")
 public class ProductController {
 
     private final ProductService productService;
     private final ProductUsecase productUsecase;
 
+    @Operation(
+            summary = "상품목록 조회",
+            description = "상품목록을 조회한다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "상품 목록 조회 성공",
+                            content = @Content(schema = @Schema(implementation = ProductsResponseDTO.class)))
+            }
+    )
     @GetMapping("/api/products")
     public ResponseEntity<List<ProductsResponseDTO>> getProducts(
             @Valid @RequestParam(defaultValue = "0") int page,
@@ -48,6 +63,14 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(resultProducts);
     }
 
+    @Operation(
+            summary = "상품상세 조회",
+            description = "상품상세를 조회한다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "상품 상세 조회 성공",
+                            content = @Content(schema = @Schema(implementation = ProductResponseDTO.class)))
+            }
+    )
     @GetMapping("/api/products/{productId}")
     public ResponseEntity<ProductResponseDTO> getProduct(
             @PathVariable(name = "productId") int productId) {
@@ -63,6 +86,14 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(resultProduct);
     }
 
+    @Operation(
+            summary = "상위상품 조회",
+            description = "많이팔린 상위3개 상품을 조회한다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "상위상품 조회 성공",
+                            content = @Content(schema = @Schema(implementation = ProductsResponseDTO.class)))
+            }
+    )
     @GetMapping("/api/products/topProduct")
     public ResponseEntity<List<ProductsResponseDTO>> getTopProduct() {
 
