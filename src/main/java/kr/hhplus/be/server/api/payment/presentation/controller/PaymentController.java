e9e445af846f6ce4d61e5dc99476ad0b5990ccdc
홍준영 <hongjy1997@naver.com>
2025-01-09 16:22:27 +0900

    :sparkles: feat : 결제 기능 / 단위 테스트

--- src/main/java/kr/hhplus/be/server/api/payment/presentation/controller/PaymentController.java
+++ src/main/java/kr/hhplus/be/server/api/payment/presentation/controller/PaymentController.java
@@ -14,28 +14,8 @@ public class PaymentController {
     @PostMapping("/api/payments/")
     public ResponseEntity<PaymentResponseDTO> payments(
             @RequestBody PaymentRequestDTO paymentRequestDTO) {
-/*
-
-        PaymentResponseDTO response = new PaymentResponseDTO();
-        response.setPaymentId(1);
-        response.setPaymentResult(true);
-
-        if(paymentRequestDTO.getUserId() <= 0) {
-            response.setMessage("존재하지 않는 유저입니다.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
-
-        if(paymentRequestDTO.getOrderId() <= 0) {
-            response.setMessage("존재하지 않는 유저입니다.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
-
-        if(paymentRequestDTO.getUserId() == 10 && paymentRequestDTO.getOrderId() == 10) { //에러를 받기위해 임시로 작성한
-            response.setMessage("만료된 주문입니다.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
-
-*/
+        
+        //TODO usecase 작성 예정
 
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
